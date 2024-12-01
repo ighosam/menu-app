1832f8e7070ff34cf7b586730ecb74994fe331ef
Sam <sam@Sams-MBP.localdomain>
2024-12-01 00:37:20 -0500

    modified logo.tsx to fix error

--- components/svgs/logo.tsx
+++ components/svgs/logo.tsx
@@ -5,7 +5,7 @@ export const Icons ={
   logo:(props?:any)=>{
     const {className,fill,...rest} = props
     return (
-<svg {...rest} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={fill} stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className={cn("lucide lucide-align-justify",className)}><path d="M3 12h18"/><path d="M3 18h18"/><path d="M3 6h18"/></svg>
+<svg {...rest} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={fill} stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={cn("lucide lucide-align-justify",className)}><path d="M3 12h18"/><path d="M3 18h18"/><path d="M3 6h18"/></svg>
     )
   },
    car:(props?:any)=>{
